daba854835696772685859dcbcaf50dad78bf3fc
Nick Camporillo <cbor_spambloc@yahoo.com>
2018-08-12 23:57:53 -0400

    Delete XmlTransformerCommandLine.bat

--- XML/Sample/XmlTransformerCommandLine.bat
+++
@@ -1,4 +0,0 @@
-c:
-cd C:\MyStuff\Hobbies\HobbyTools\HobbyApps\Conlangs\src\XmlTransformerCommandLine\Sample
-
-XmlTransformerCommandLine.exe /xml="Data\MinhastLex.xml" /xsl="stylesheets\TransformLexicon.xsl" /out="output\NewLexiconFormat.xml"
