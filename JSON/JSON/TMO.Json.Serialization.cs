1c27cd5e1ac254ddfa1a21c1d5ad3341d2a533c2
Nick Camporillo <cbor_spambloc@yahoo.com>
2018-08-12 22:01:22 -0400

    Add files via upload

---
+++ JSON/JSON/TMO.Json.Serialization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+using WTF.Core.FileSystem;
+
+namespace TMO.Json
+{
+    public class Serialization
+    {
+        private static FileSystemManager fmgr = new FileSystemManager();
+
+        public static string SerializeObjectToJson(object objectToSerialize)
+        {
+            string jsonResultString = JsonConvert.SerializeObject(objectToSerialize);
+
+            return jsonResultString;
+        }
+
+        public static void SerializeObjectToJsonFile(object objectToSerialize, string jsonFiile)
+        {
+            string jsonResults = JsonConvert.SerializeObject(objectToSerialize);
+
+            fmgr.WriteFile(jsonFiile, jsonResults);
+        }
+
+        public static T DeserializeJsonFileToObject<T>(string jsonFileName)
+        {
+            string jsonData = fmgr.ReadFile(jsonFileName);
+
+            T rawParsed = JsonConvert.DeserializeObject<T>(jsonData);
+            return rawParsed;
+        }
+    }
+}
