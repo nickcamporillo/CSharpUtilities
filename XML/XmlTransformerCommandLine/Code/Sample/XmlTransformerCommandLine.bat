b4e0ea19d49ad46bd496c1d23c499117cc217c0c
Nick Camporillo <cbor_spambloc@yahoo.com>
2018-08-12 23:53:37 -0400

    Add files via upload

---
+++ XML/XmlTransformerCommandLine/Code/Sample/XmlTransformerCommandLine.bat
@@ -0,0 +1,4 @@
+c:
+cd C:\MyStuff\Hobbies\HobbyTools\HobbyApps\Conlangs\src\XmlTransformerCommandLine\Sample
+
+XmlTransformerCommandLine.exe /xml="Data\MinhastLex.xml" /xsl="stylesheets\TransformLexicon.xsl" /out="output\NewLexiconFormat.xml"
